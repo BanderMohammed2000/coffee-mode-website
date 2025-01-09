2d9083b3bff547160075f67be1460b3de008273c
Bander Btati <bander.mohammed.2000@gmail.com>
2025-01-09 04:13:05 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+import "bootstrap-v5-rtl/dist/js/bundle.min.js";
+import "bootstrap-v5-rtl/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/popper.min";
+import $ from "jquery";
+import "../sass/style.scss";
+import "@fortawesome/fontawesome-free/js/all.min";
+
+// $(document).ready(() => {
+//   $("img").css("width", "50%");
+// });
---
+++ webpack.config.js
@@ -0,0 +1,118 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+module.exports = {
+  stats: "errors-only",
+  entry: "./src/js/index.js",
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "build"),
+    filename: "js/bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "../",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+
+      // {
+      //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //   exclude: /fonts/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+
+      //       options: {
+      //         name: "[name].[ext]",
+
+      //         outputPath: "images",
+      //       },
+      //     },
+      //   ],
+      // },
+      // {
+      //   test: /\.(svg|eot|woff|woff2|ttf)$/,
+      //   exclude: /images/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+
+      //       options: {
+      //         name: "[name].[ext]",
+
+      //         outputPath: "fonts",
+      //       },
+      //     },
+      //   ],
+      // },
+
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        exclude: /images/,
+        // type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        exclude: /fonts/,
+        // type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+
+      {
+        test: /\.html$/i,
+
+        loader: "html-loader",
+      },
+
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
+      },
+    ],
+  },
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, " build"),
+    },
+
+    port: 9000,
+
+    hot: false,
+    // liveReload: true,
+
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: "css/style.css",
+    }),
+  ],
+};
